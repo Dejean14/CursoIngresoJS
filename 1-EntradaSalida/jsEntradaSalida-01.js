10084253cf0ab183fe633b9febba2a26831217f0
dejean14 <pablodejean@outlook.com>
2022-01-30 22:04:36 -0300

    pruena n°10000

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("le agarre la onda octa");
+	alert("hola mundo");
 }
 
